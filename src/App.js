eb50084512b9b691f9a74fd5c9984a9e621b11e6
bala201 <61304466+bala201@users.noreply.github.com>
2021-05-16 13:42:04 +0530

    all files are added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Bala from './Bala.js';
+import shoe1 from './vadivelu.jpg'
+
+
+function App() {
+  return (
+    <div className="Appsfasg">
+    <Bala />
+    <header className="App-header"> 
+    
+     <img src={shoe1} alt=""/>
+         <p>hukbdfsjlh</p>
+          {/* <p>bkzvjkbdfkjzlb</p>*/}
+         </header>
+         <footer className="app">
+         <img src={shoe1} alt=""/></footer>
+    <p>vjkdsbvk</p>
+     </div>
+  );
+
+ 
+}
+
+export default App;
---
+++ src/Bala.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import shoe1 from './bala.jpg';
+
+import PropTypes from 'prop-types';
+
+class Bala extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className="App-logo">
+                    HI
+                    <img src={shoe1} alt=""/>
+            </div>
+        );
+    }
+}
+
+Bala.propTypes = {
+
+};
+
+export default Bala;
